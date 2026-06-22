98614c48e18163b164bfe7828113cce5a3666231
Мирва <nomirva@MacBook-Air-Mirva.local>
2026-06-22 22:30:16 +0300

    Add examples

---
+++ examples/lib-math/include/math_util.h
@@ -0,0 +1,7 @@
+#ifndef MATH_UTIL_H
+#define MATH_UTIL_H
+
+int add(int a, int b);
+int mul(int a, int b);
+
+#endif
---
+++ examples/lib-math/src/math_util.c
@@ -0,0 +1,9 @@
+#include "math_util.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
