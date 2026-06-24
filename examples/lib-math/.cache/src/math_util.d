612cb49bcc613bbc23db3de9c77f9f792164513a
Мирва <branarx@gmail.com>
2026-06-24 18:09:37 +0300

    Remove trash

--- examples/lib-math/.cache/src/math_util.d
+++
@@ -1,3 +0,0 @@
-/Users/nomirva/repos/shard/examples/lib-math/.cache/src/math_util.o: \
-  /Users/nomirva/repos/shard/examples/lib-math/src/math_util.c \
-  /Users/nomirva/repos/shard/examples/lib-math/include/math_util.h
