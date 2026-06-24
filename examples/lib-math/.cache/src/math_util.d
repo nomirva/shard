cf8f164a0d1155def0c7d234536cd2230af466ea
Мирва <nomirva@MacBook-Air-Mirva.local>
2026-06-24 17:34:58 +0300

    Fix cache and change naming

---
+++ examples/lib-math/.cache/src/math_util.d
@@ -0,0 +1,3 @@
+/Users/nomirva/repos/shard/examples/lib-math/.cache/src/math_util.o: \
+  /Users/nomirva/repos/shard/examples/lib-math/src/math_util.c \
+  /Users/nomirva/repos/shard/examples/lib-math/include/math_util.h
