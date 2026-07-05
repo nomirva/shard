c969d21c0998fbc5665f9f1f9798cb75b8be576b
Мирва <branarx@gmail.com>
2026-07-06 02:21:56 +0300

    Unified target/ output, --def flag, shard run, centralized .shard/

--- examples/app/src/main.c
+++ examples/app/src/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#if defined(GREET)
 #include "greet.h"
+#elif defined(MATH)
 #include "math_util.h"
+#endif
 
 int main(void) {
-    greet("world");
-
+#if defined(GREET)
+    greet("shard");
+    return 0;
+#elif defined(MATH)
     printf("2 + 3 = %d\n", add(2, 3));
     printf("4 * 5 = %d\n", mul(4, 5));
-
     return 0;
+#else
+#error "No variant selected. Build with: --def GREET | --def MATH"
+#endif
 }
